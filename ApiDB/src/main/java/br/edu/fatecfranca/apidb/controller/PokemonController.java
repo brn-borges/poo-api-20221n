7e602759ce890a3a59f801f43db20d0311d490f7
Bruno Borges <bruninho-borgesf@hotmail.com>
2022-05-20 20:57:23 -0300

    Aula 20/05/2022

--- ApiDB/src/main/java/br/edu/fatecfranca/apidb/controller/PokemonController.java
+++ ApiDB/src/main/java/br/edu/fatecfranca/apidb/controller/PokemonController.java
@@ -3,7 +3,12 @@ package br.edu.fatecfranca.apidb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.fatecfranca.apidb.model.Pokemon;
@@ -24,5 +29,31 @@ public class PokemonController {
 		// select * from pokemon
 		return injecao.findAll();
 	}
+	
+	//metodo post para inserir um pokemon na rota /pokemon
+	@PostMapping("/pokemon")
+	public Pokemon add (@RequestBody Pokemon pokemon) {
+		Pokemon novoPokemon = injecao.save(pokemon);
+		return novoPokemon;
+	}
+	
+	//metodo delete para remover um pokemon na tara /pokemon
+	@DeleteMapping("/pokemon/{id}")
+	public String remove(@PathVariable Long id) {
+		try { // tenta remover
+			injecao.deleteById(id);
+			return "Remoção com sucesso";
+		}catch(Exception e) { //pegar o erro
+			return "Pokemon não encontrado para remoção!";
+		}
+	}
+	
+	@PutMapping("/pokemon")
+	public Pokemon updates (@RequestBody Pokemon pokemon) {
+		Pokemon alteradoPokemon = injecao.save(pokemon);
+		//pokemon tera id, portanto o save atualiza
+		return alteradoPokemon;
+	}
+	
 	
 }
